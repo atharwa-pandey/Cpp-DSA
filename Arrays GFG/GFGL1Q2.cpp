67a319f69b1da38a799e5c26790eb3de4dec2e6c
atharwa-pandey <76654647+atharwa-pandey@users.noreply.github.com>
2021-06-22 10:29:35 +0530

    Update GFGL1Q2.cpp

--- Arrays GFG/GFGL1Q2.cpp
+++ Arrays GFG/GFGL1Q2.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 
 using namespace std;
+void bubble_sort(int arr[],int size_of_input);
 
 int main(){
     int size_of_input {};
@@ -11,14 +12,29 @@ int main(){
     for(int i {}; i < size_of_input; i++){
         cin>>array[i];
     }
-    for(int i {};i < size_of_input-1; i++){
+   bubble_sort(array,size_of_input);
+    cout<<"Maximum number is "<<array[0]<<endl;
+    cout<<"Minimum number is "<<array[size_of_input-1]<<endl;
+    for(auto var:array){
+        cout<<var<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
+
+void bubble_sort(int array[],int size_of_input){
+    int count {};
+     for(int i {};i < size_of_input-1; i++){
         if(array[i]<array[i+1]){
             int temp {array[i]};
             array[i]= array[i+1];
             array[i+1]= temp;
+            count++;
+        }
+        if(count!=0){
+            bubble_sort(array,size_of_input);
         }
     }
-    cout<<"Maximum number is "<<array[0]<<endl;
-    cout<<"Minimum number is "<<array[size_of_input-1]<<endl;
-    return 0;
+
+
 }
